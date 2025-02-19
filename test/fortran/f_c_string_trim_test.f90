99ed37a66c12502659860c5544ee8acbcae0fe85
amstokely <andystokely@gmail.com>
2025-02-19 12:17:46 -0700

    Fixed string trim bug in f_c_string_t_mod

---
+++ test/fortran/f_c_string_trim_test.f90
@@ -0,0 +1,36 @@
+subroutine f_c_string_t_to_c_trim_test()
+    use test_utils_mod
+    use f_c_string_t_mod
+    use iso_c_binding
+    implicit none
+    integer :: status
+
+    type(f_c_string_t) :: f_c_string
+    type(c_ptr) :: c_string
+    integer :: n
+    c_string = f_c_string%to_c("abc   ")
+    n = c_strlen(c_string)
+    call assertEqual(3, n, status, assert)
+end subroutine
+
+subroutine f_c_string_t_to_f_trim_test()
+    use test_utils_mod
+    use f_c_string_t_mod
+    use iso_c_binding
+    implicit none
+    integer :: status
+
+    type(f_c_string_t) :: f_c_string
+    type(c_ptr) :: c_string
+    character(len=:), allocatable :: f_string
+    c_string = f_c_string%to_c("abc   ")
+    f_string = f_c_string%to_f(c_string)
+    call assertEqual("abc   ", f_string, status, assert)
+end subroutine
+
+program netcdf_file_test
+
+    call f_c_string_t_to_c_trim_test()
+    call f_c_string_t_to_f_trim_test()
+
+end program netcdf_file_test
