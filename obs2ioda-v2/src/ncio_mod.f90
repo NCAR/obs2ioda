e10537170ab05afe7ca86d108f90c80a37df0d65
amstokely <andystokely@gmail.com>
2025-03-11 14:32:49 -0600

    Added ws around if statement ( and ).

--- obs2ioda-v2/src/ncio_mod.f90
+++ obs2ioda-v2/src/ncio_mod.f90
@@ -137,7 +137,7 @@ subroutine write_obs (filedate, write_opt, outdir, itim)
 
       status = netcdfAddDim(netcdfID, trim(ncname), val_ncdim(1), ncid_ncdim(1))
       status = netcdfPutAtt(netcdfID, trim(ncname), val_ncdim(ncid_ncdim(1)))
-      if (trim(ncname) == 'nchans') then
+      if ( trim(ncname) == 'nchans' ) then
          status = netcdfAddVar(netcdfID, trim(ncname), NF90_INT, 1, [trim(ncname)], fillValue = -999)
       else
          status = netcdfAddVar(netcdfID, trim(ncname), NF90_INT, 1, [trim(ncname)])
