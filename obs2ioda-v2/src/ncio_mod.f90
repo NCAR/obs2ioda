5fd730d314c635a3c42a0c674f0ca90ff6276dee
amstokely <andystokely@gmail.com>
2025-03-13 16:57:39 -0600

    Simplified netcdfPutAtt call in write_obs.

--- obs2ioda-v2/src/ncio_mod.f90
+++ obs2ioda-v2/src/ncio_mod.f90
@@ -141,7 +141,7 @@ subroutine write_obs (filedate, write_opt, outdir, itim)
 
       do i = 2, n_ncdim
          status = netcdfAddDim(netcdfID, trim(name_ncdim(i)), val_ncdim(i), ncid_ncdim(i))
-         status = netcdfPutAtt(netcdfID, trim(name_ncdim(ncid_ncdim(i))), val_ncdim(i))
+         status = netcdfPutAtt(netcdfID, trim(name_ncdim(i)), val_ncdim(i))
          status = netcdfAddVar(netcdfID, trim(name_ncdim(i)), NF90_INT, 1, [trim(ncname)])
       end do
 
