1f11a66756dd22f89e3fd9e987fe80a5a4850d52
amstokely <andystokely@gmail.com>
2025-03-18 14:50:47 -0600

    Replaced harded coded path for ioda schema yaml with macro set in CMake.

--- obs2ioda-v2/src/cxx/ioda_schema.cc
+++ obs2ioda-v2/src/cxx/ioda_schema.cc
@@ -4,16 +4,20 @@ IodaSchema::IodaSchema(YAML::Node schema) {
     for (auto attribute: schema["Attributes"]) {
         if (attribute["Attribute"]) {
             auto iodaAttribute = std::make_shared<IodaAttribute>();
-            iodaAttribute->name = attribute["Attribute"].begin()->as<std::string>();
+            iodaAttribute->name = attribute["Attribute"].begin()->as<
+                std::string>();
             for (auto attr: attribute["Attribute"]) {
-                this->attributes[attr.as<std::string>()] = iodaAttribute;
+                this->attributes[attr.as<std::string>()] =
+                        iodaAttribute;
             }
         }
     }
     for (auto variable: schema["Variables"]) {
         if (variable["Variable"]) {
-            std::shared_ptr<IodaVariable> iodaVariable = std::make_shared<IodaVariable>();
-            iodaVariable->name = variable["Variable"].begin()->as<std::string>();
+            std::shared_ptr<IodaVariable> iodaVariable =
+                    std::make_shared<IodaVariable>();
+            iodaVariable->name = variable["Variable"].begin()->as<
+                std::string>();
             for (auto var: variable["Variable"]) {
                 this->variables[var.as<std::string>()] = iodaVariable;
             }
@@ -23,9 +27,13 @@ IodaSchema::IodaSchema(YAML::Node schema) {
     for (auto dimension: schema["Dimensions"]) {
         if (dimension["Dimension"]) {
             auto iodaDimension = std::make_shared<IodaDimension>();
-            iodaDimension->name = dimension["Dimension"].begin()->as<std::string>();
+            auto iodaVariable = std::make_shared<IodaVariable>();
+            iodaDimension->name = dimension["Dimension"].begin()->as<
+                std::string>();
+            iodaVariable->name = iodaDimension->name;
             for (auto dim: dimension["Dimension"]) {
                 this->dimensions[dim.as<std::string>()] = iodaDimension;
+                this->variables[dim.as<std::string>()] = iodaVariable;
             }
         }
     }
@@ -41,48 +49,51 @@ IodaSchema::IodaSchema(YAML::Node schema) {
     }
 }
 
-std::shared_ptr<IodaAttribute> IodaSchema::getAttribute(const std::string& name) {
+std::shared_ptr<IodaAttribute> IodaSchema::getAttribute(
+    const std::string &name
+) {
     std::shared_ptr<IodaAttribute> iodaAttribute;
     if (this->attributes.find(name) != this->attributes.end()) {
         iodaAttribute = this->attributes[name];
-    }
-    else {
+    } else {
         iodaAttribute = std::make_shared<IodaAttribute>();
         iodaAttribute->name = name;
     }
     return iodaAttribute;
 }
 
-std::shared_ptr<IodaVariable> IodaSchema::getVariable(const std::string& name) {
+std::shared_ptr<IodaVariable> IodaSchema::getVariable(
+    const std::string &name
+) {
     std::shared_ptr<IodaVariable> iodaVariable;
     if (this->variables.find(name) != this->variables.end()) {
         iodaVariable = this->variables[name];
-    }
-    else {
+    } else {
         iodaVariable = std::make_shared<IodaVariable>();
         iodaVariable->name = name;
     }
     return iodaVariable;
 }
 
-std::shared_ptr<IodaDimension> IodaSchema::getDimension(const std::string& name) {
+std::shared_ptr<IodaDimension> IodaSchema::getDimension(
+    const std::string &name
+) {
     std::shared_ptr<IodaDimension> iodaDimension;
     if (this->dimensions.find(name) != this->dimensions.end()) {
         iodaDimension = this->dimensions[name];
-    }
-    else {
+    } else {
         iodaDimension = std::make_shared<IodaDimension>();
         iodaDimension->name = name;
     }
     return iodaDimension;
 }
 
-std::shared_ptr<IodaGroup> IodaSchema::getGroup(const std::string& name) {
+std::shared_ptr<IodaGroup>
+IodaSchema::getGroup(const std::string &name) {
     std::shared_ptr<IodaGroup> iodaGroup;
     if (this->groups.find(name) != this->groups.end()) {
         iodaGroup = this->groups[name];
-    }
-    else {
+    } else {
         iodaGroup = std::make_shared<IodaGroup>();
         iodaGroup->name = name;
     }
--- obs2ioda-v2/src/cxx/netcdf_file.cc
+++ obs2ioda-v2/src/cxx/netcdf_file.cc
@@ -2,8 +2,13 @@
 #include "netcdf_error.h"
 #include <memory>
 
+#ifndef IODA_SCHEMA_YAML
+#define IODA_SCHEMA_YAML
+#endif
+
 namespace Obs2Ioda {
-    IodaSchema iodaSchema(YAML::LoadFile("/home/astokely/projects/obs2ioda/share/ObsSpace.yaml"));
+    const std::string iodaSchemaYaml = std::string(IODA_SCHEMA_YAML);
+    IodaSchema iodaSchema(YAML::LoadFile(iodaSchemaYaml));
 
     FileMap &FileMap::getInstance() {
         static FileMap instance;
